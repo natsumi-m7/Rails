f0d6dd8cf76123a7b529cd41deba30b934a77d52
natsumi-m7 <n.osada0724@gmail.com>
2019-01-13 14:53:13 +0900

    [Finish] 3-9

---
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '7f215b14144eb0aec98bfcdaabe99d9e4e8449428a95f8274ef6e55b2958a9a68c6201163e77d46e85ed4814247635e02c576a0f25688f1121151b998be1c483'
+
